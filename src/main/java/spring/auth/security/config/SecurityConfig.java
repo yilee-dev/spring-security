a2038d8aa763f40412e756cab110f4d0bc84909f
youngin.lee <devyilee@gmail.com>
2025-02-21 15:47:10 +0900

    Create WebAuthenticationDetailsSource

--- src/main/java/spring/auth/security/config/SecurityConfig.java
+++ src/main/java/spring/auth/security/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import spring.auth.security.details.FormWebAuthenticationDetailsSource;
 
 @EnableWebSecurity
 @Configuration
@@ -21,6 +22,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
+    private final FormWebAuthenticationDetailsSource authenticationDetailsSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,7 +30,10 @@ public class SecurityConfig {
                         .requestMatchers("/css/**", "/js/**", "/images/**", "/favicon.*", "/*/icon-*").permitAll()
                         .requestMatchers("/", "/signup").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(form -> form.loginPage("/login").permitAll())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .authenticationDetailsSource(authenticationDetailsSource)
+                        .permitAll())
                 .authenticationProvider(authenticationProvider);
 
         return http.build();
---
+++ src/main/java/spring/auth/security/details/FormWebAuthenticationDetailsSource.java
@@ -0,0 +1,14 @@
+package spring.auth.security.details;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FormWebAuthenticationDetailsSource implements AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> {
+    @Override
+    public WebAuthenticationDetails buildDetails(HttpServletRequest request) {
+        return new FormWebAuthenticationDetails(request);
+    }
+}
